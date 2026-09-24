e878cac302e4df377a423d6d8b9c950be5e346ef
virendra kumar <virendrajangir563@gmail.com>
2026-09-24 18:21:53 +0530

    Connect frontend to production backend

--- level3-task1/frontend/src/services/api.js
+++ level3-task1/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: "https://codveda-fullstack-internship-j933.onrender.com/api",
 });
 
 export default api;
